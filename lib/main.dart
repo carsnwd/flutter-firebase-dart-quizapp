543bdf7842405ba6153c1690280f24d397e2b9fc
carsn <carsnwd@gmail.com>
2020-11-22 12:30:07 -0500

    Layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'dart:io' show Platform;
 
 /// All Dart code requires a main function. We pass in the flutter MyApp widget here.
 void main() {
@@ -13,17 +12,40 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     /// MaterialApp is just the root app context for a Flutter app, it defines things like routes, themes, analytics etc.
     return MaterialApp(
-      /// Scaffold can be used as a blank canvas but scaffold out things like drawers, floating actions, etc.
+      // //Centering a widget
+      // home: Scaffold(
+      //   appBar: AppBar(title: Text("My Cool App")),
+      //   body: Center(
+      //     child: Icon(Icons.cake),
+      //   ),
+      // ),
+
+      // //Align widget
+      // home: Scaffold(
+      //   appBar: AppBar(title: Text("My Cool App")),
+      //   body: Align(
+      //     alignment: Alignment.centerRight,
+      //     child: Icon(Icons.cake),
+      //   ),
+      // ),
+
+      // //Padding widget - Uses EdgeInsets to define which padding and pixels
+      // home: Scaffold(
+      //   appBar: AppBar(title: Text("My Cool App")),
+      //   body: Padding(
+      //     padding: const EdgeInsets.all(8.0),
+      //     child: Icon(Icons.cake),
+      //   ),
+      // ),
+
+      //Container widget can combine many layout widgets into one, but not as readable
       home: Scaffold(
         appBar: AppBar(title: Text("My Cool App")),
-        body: Center(
+        body: Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(8.0),
           child: Icon(Icons.cake),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-        ),
-        drawer: Drawer(),
-        bottomNavigationBar: BottomAppBar(),
       ),
     );
   }
