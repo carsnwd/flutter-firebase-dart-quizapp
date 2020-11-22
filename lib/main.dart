bffe60d07a8f1b48abe7d4081924daf957bdd28f
carsn <carsnwd@gmail.com>
2020-11-22 11:10:03 -0500

    Platform Checking

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:io' show Platform;
 
 /// All Dart code requires a main function. We pass in the flutter MyApp widget here.
 void main() {
@@ -10,55 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        // home: Scaffold(body: MyFirstStatefulWidget()),
-        home: Scaffold(body: MyFirstStatelessWidget(color: Colors.blue)));
-  }
-}
-
-/// Stateless Widget is a Flutter widget that is not mutable. This one just takes a color and makes a box on the center of the screen.
-class MyFirstStatelessWidget extends StatelessWidget {
-  final Color color;
-
-  MyFirstStatelessWidget({this.color});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(width: 50, height: 50, color: color),
-    );
-  }
-}
-
-/// Stateful widget is the opposite, a Flutter widget that is mutable.
-class MyFirstStatefulWidget extends StatefulWidget {
-  @override
-  _MyFirstStatefulWidgetState createState() => _MyFirstStatefulWidgetState();
-}
-
-class _MyFirstStatefulWidgetState extends State<MyFirstStatefulWidget> {
-  int count = 0;
-  @override
-  void initState() {
-    /// This is like ngInit to initialize the widget.
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    /// This is like ngDestory to run when the widget is destroyed.
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: FlatButton(
-          child: Text("$count"),
-          onPressed: () {
-            /// This is similar to React, telling the app the state changed and to recall the build function.
-            setState(() {
-              count++;
-            });
-          }),
+      home: Scaffold(
+        body: Center(
+          ///Flutter can use default UI and switch between Android Material or iOS Cupertino depending on the platform.
+          child: Platform.isAndroid ? Switch(value: true, onChanged: (v) => null) : CupertinoSwitch(value: true, onChanged: (v) => null)
+        ),
+      ),
     );
   }
 }
